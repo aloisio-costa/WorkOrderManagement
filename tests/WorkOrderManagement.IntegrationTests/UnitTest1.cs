944fab8fa171ffe26d1f6623a8b6c4ec11845887
aloisio-costa <aloisiocosta93@gmail.com>
2026-04-13 19:51:50 +0100

    chore: scaffold solution structure with clean architecture layers

---
+++ tests/WorkOrderManagement.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkOrderManagement.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/WorkOrderManagement.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkOrderManagement.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
