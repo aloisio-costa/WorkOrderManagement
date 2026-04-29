673e2696bda9e2916e9615a31ee1752e9afe7acc
aloisio-costa <aloisiocosta93@gmail.com>
2026-04-29 17:57:48 +0100

    Add outbox messages migration

--- src/WorkOrderManagement.Infrastructure/Persistence/Migrations/20260429165424_AddOutboxMessages.Designer.cs
+++ src/WorkOrderManagement.Infrastructure/Persistence/Migrations/20260429165424_AddOutboxMessages.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WorkOrderManagement.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using WorkOrderManagement.Infrastructure.Persistence;
 namespace WorkOrderManagement.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260429165424_AddOutboxMessages")]
+    partial class AddOutboxMessages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
